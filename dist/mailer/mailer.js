b885b2dca9648e73eb3212e405239540aca3e1e1
Nao <dragonarcher2005@gmail.com>
2023-12-06 02:34:00 -0300

    Working dotenv

--- dist/mailer/mailer.js
+++ dist/mailer/mailer.js
@@ -14,6 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendEmail = void 0;
 const nodemailer_1 = __importDefault(require("nodemailer"));
+require('dotenv').config();
 const transporter = nodemailer_1.default.createTransport({
     service: 'gmail',
     auth: {
--- mailer/mailer.ts
+++ mailer/mailer.ts
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer"
-import dotenv from 'dotenv'
+
+require('dotenv').config()
 
 const transporter = nodemailer.createTransport({
     service:'gmail',
